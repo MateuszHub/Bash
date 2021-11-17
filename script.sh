243a26eb9ecb300d2482ec9879256a7c9daaa338
Mateusz <mateusz.n.mail@gmail.com>
2021-11-17 19:46:26 +0100

    save game and bot

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# PLANSZA={"2"," "," ","2"," "," ","2"," "," ",}
-PLANSZA=(" " " " " " " " " " " " " " " " " ")
+PLANSZA=(" " " " " " " " " " " " " " " " " " 9)
 KONIEC=0
 GRACZ=1
+TRYB=""
+PLIK_Z_ZAPISAMI=./saves
+
 function wyswietl {
     clear 
     echo "Plansza"
@@ -21,22 +23,22 @@ function sprawdz_wygrana {
     for POLE in {0..2}
     do
         let INDEX=POLE 
-        if [[ ${PLANSZA[$INDEX]} -ne " " && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 3]} 
+        if [[ ${PLANSZA[$INDEX]} -ne 0 && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 3]} 
          && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 6]} ]]
         then
             let KONIEC=$GRACZ
         fi
         let INDEX=POLE*3 
-        if [[ ${PLANSZA[$INDEX]} -ne " " && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 1]} 
+        if [[ ${PLANSZA[$INDEX]} -ne 0 && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 1]} 
          && ${PLANSZA[$INDEX]} -eq ${PLANSZA[$INDEX + 2]} ]]
         then
             let KONIEC=$GRACZ
         fi
-        if [[ ${PLANSZA[0]} -ne " " && ${PLANSZA[0]} -eq ${PLANSZA[4]}  && ${PLANSZA[0]} -eq ${PLANSZA[8]} ]]
+        if [[ ${PLANSZA[0]} -ne 0 && ${PLANSZA[0]} -eq ${PLANSZA[4]}  && ${PLANSZA[0]} -eq ${PLANSZA[8]} ]]
         then
             let KONIEC=$GRACZ
         fi
-        if [[ ${PLANSZA[6]} -ne " " && ${PLANSZA[6]} -eq ${PLANSZA[4]}  && ${PLANSZA[4]} -eq ${PLANSZA[2]} ]]
+        if [[ ${PLANSZA[6]} -ne 0 && ${PLANSZA[6]} -eq ${PLANSZA[4]}  && ${PLANSZA[4]} -eq ${PLANSZA[2]} ]]
         then
             let KONIEC=$GRACZ
         fi
@@ -48,21 +50,119 @@ function zmiana_gracza {
 }
 
 function zaznacz {
-    echo $1
     let PLANSZA[$1]="${GRACZ}"
 }
 
-while [ $KONIEC == 0 ]
-do
-    wyswietl
-    echo "KOLEJ GRACZA ${GRACZ}"
-    read POLE
-    if [[ $POLE -lt 9 && $POLE -ge 0 && ${PLANSZA[$POLE]} -eq " " ]]
+function zapisz_informacje_o_trybie {
+    if [[ $TRYB == "B" ]]
+    then
+        let PLANSZA[9]="8"
+    else
+        let PLANSZA[9]="9"
+    fi
+}
+
+function wczytaj_informacje_o_trybie {
+    if [[ ${PLANSZA[9]} -eq 8 ]]
+    then
+        TRYB="B"
+    else
+        TRYB="H"
+    fi
+}
+
+function zapisz_stan {
+    zapisz_informacje_o_trybie
+    printf "%s\n" "${PLANSZA[@]}" > ${PLIK_Z_ZAPISAMI}
+    echo "Zapisano!"
+}
+
+function wczytaj_stan {
+    readarray -t PLANSZA < ${PLIK_Z_ZAPISAMI}      
+    wczytaj_informacje_o_trybie
+    ustal_gracza
+}
+
+function ustal_gracza {
+    local LICZNIK_TUR=0;
+    for I in {0..8}
+    do
+        if  [[ ${PLANSZA[$I]} -gt 0 ]]
+        then 
+            let LICZNIK_TUR=$(($LICZNIK_TUR+1));
+        fi
+    done
+    echo $LICZNIK_TUR
+    
+    let GRACZ=($LICZNIK_TUR)%2+1 
+    echo $GRACZ
+    echo $TRYB
+}
+
+
+function wyborStanuGry {
+    if [[ -s "$PLIK_Z_ZAPISAMI" ]]
+    then
+        echo "Czy chcesz wczytać zapis? T/N"
+        read ODPOWIEDZ
+        if [ $ODPOWIEDZ == "T" ]
+        then
+            wczytaj_stan
+        else 
+            nowa_gra
+        fi
+    else
+        nowa_gra
+    fi         
+
+}
+
+function nowa_gra {
+    while [[ $TRYB != "B" && $TRYB != "H" ]] 
+    do
+        echo "Wybierz tryb gry: B - (bot)gra z komputerem, H - hot seat"
+        read TRYB
+    done    
+}
+function standardowa_obsluga {
+    if [[ $1 -lt 9 && $1 -ge 0 && ${PLANSZA[$1]} -eq " " ]]
     then 
-        zaznacz $POLE
+        zaznacz $1
         sprawdz_wygrana
         wyswietl
         zmiana_gracza
+    fi
+}
+function obsluga_czlowieka {
+    
+        echo "Aby zapisać stan gry wpisz S"
+        echo "KOLEJ GRACZA ${GRACZ}"
+        read WEJSCIE
+        
+        standardowa_obsluga $WEJSCIE
+        
+        if [[ $WEJSCIE -eq "S" ]]
+        then
+            zapisz_stan
+        fi
+}
+
+function obsluga_bota {
+        echo "KOLEJ BOTA ${GRACZ}"
+        standardowa_obsluga $(($RANDOM%9))
+    
+}
+
+wyborStanuGry
+while [ $KONIEC == 0 ]
+do
+    wyswietl
+    if [[ $GRACZ -eq 1 || $TRYB == "H" ]]
+    then
+        obsluga_czlowieka
+    elif [[ $TRYB == "B" && $GRACZ -eq 2 ]]
+    then
+        obsluga_bota
     fi
 done
 zmiana_gracza
